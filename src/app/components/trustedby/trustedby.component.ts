79665c9d816f88dbdbb7ecfba0b6c783865f0ff8
fbd3887 <sky.blue.1991519@gmail.com>
2021-05-12 14:57:26 -0700

    landing page UI update 2

---
+++ src/app/components/trustedby/trustedby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trustedby',
+  templateUrl: './trustedby.component.html',
+  styleUrls: ['./trustedby.component.scss']
+})
+export class TrustedbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
